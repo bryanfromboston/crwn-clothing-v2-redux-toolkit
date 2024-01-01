97f3a6b8d55d6d04339bd33d9c101b573f9ad574
Bryan Maleszyk <bryan@maleszyk.com>
2024-01-01 11:35:04 -0500

    Wrapped up Stripe Payments

--- src/store/store.js
+++ src/store/store.js
@@ -1,34 +1,39 @@
-import { compose, createStore, applyMiddleware } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import logger from 'redux-logger';
+import { configureStore } from '@reduxjs/toolkit'
 
-import { rootReducer } from './root-reducer';
+// import { compose, createStore, applyMiddleware } from 'redux';
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+import logger from 'redux-logger'
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
-  Boolean
-);
+import { rootReducer } from './root-reducer'
 
-const composeEnhancer =
-  (process.env.NODE_ENV !== 'production' &&
-    window &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(Boolean)
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  blacklist: ['user'],
-};
+// const composeEnhancer =
+//   (process.env.NODE_ENV !== 'production' &&
+//     window &&
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+//   compose;
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+//   blacklist: ['user'],
+// };
 
-const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = createStore(
-  persistedReducer,
-  undefined,
-  composedEnhancers
-);
+// const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
-export const persistor = persistStore(store);
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middleWares),
+})
+
+// export const store = createStore(
+//   persistedReducer,
+//   undefined,
+//   composedEnhancers
+// );
+
+// export const persistor = persistStore(store);
